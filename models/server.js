5ba9e4a1558e1d33eb5391ddfb1b9886c7248800
Ariel Sanchez <freska_96@hotmail.com>
2025-02-18 13:16:31 -0600

    Fin seccion 9

--- models/server.js
+++ models/server.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const cors = require('cors');
 
+const { dbConnection } = require('../database/config.db');
+
 class Server {
 
     constructor() {
@@ -9,11 +11,18 @@ class Server {
         this.port = process.env.PORT;
         this.usuariosPath = '/api/usuarios';
 
+        //~ Conectar a base de datos
+        this.conectarDB();
+
         //~ Middlewares
         this.middlewares();
 
         //~ Rutas de mi aplicacion
         this.routes();
+    };
+
+    async conectarDB() {
+        await dbConnection();
     };
 
     middlewares() {
